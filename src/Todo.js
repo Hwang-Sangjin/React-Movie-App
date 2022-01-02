4f410f9b271ef016bea3a23fc46f97e38e073050
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2022-01-02 16:36:23 +0900

    todo exercise

--- src/Todo.js
+++ src/Todo.js
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 
 function Clean() {
-    const[todo, setToDo] = useState("")
-    const [todos, setToDos] = useState([])
+    const [todo,setTodo] = useState("")
+    const [todos, setTodos] = useState([])
 
-    function onChange(event){
-        setToDo(event.target.value)
+    const onChange = (event) =>{
+        return setTodo(event.target.value)
     }
 
-    function onSubmit(event){
+    const onSubmit = (event)=>{
         event.preventDefault()
-        if(todo ==="") return
-        setToDos(currentArray =>[ todo, ...currentArray])
-        setToDo("")
+        if(todo === "") return
+        
+        setTodos((prevArr)=>[todo,...prevArr])
+        setTodo("")
     }
     console.log(todos)
 
     return(
         <div>
-            <h1>My To Dos {todos.length}</h1>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={todo} type="text" placeholder="Write your to do"></input>
-                <button>Add To Do</button>
-            </form>
-            <hr/>
-            <ul>
-                {todos.map((item, index)=> (
+        <h1>My To Dos {todos.length}</h1>
+        <form onSubmit={onSubmit}>
+            <input
+                value = {todo}
+                onChange={onChange}
+                type="string"
+                placeholder="Write your to do"
+            >
+
+            </input>
+            <button>
+                Add To Do
+            </button>
+        </form>
+        <hr/>
+        <ul>
+            {todos.map((item,index)=>{
+                return(
                     <li key={index}>{item}</li>
-                ))}
-            </ul>
+                )
+            })}
+        </ul>
+        
         </div>
+        
     )
 }
 
