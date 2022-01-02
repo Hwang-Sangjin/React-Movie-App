f9a83e7c881ef3ef58533ab95801d960316b823b
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2022-01-02 23:30:25 +0900

    movie app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Clean from './Clean'
 import ToDo from './Todo'
+import Coin from './Coin'
+import Movie from './Movie'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToDo />
+    <Movie />
   </React.StrictMode>,
   document.getElementById('root')
 );
